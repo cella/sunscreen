f8ee046c2cd53f38e752fc7dec66f5cf88cd9758
Nick Endle <nick.endle@gmail.com>
2018-08-02 16:10:49 -0500

    Add sticky nav bar on scroll

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -3,3 +3,17 @@ var inPreview = (/\/admin\/design/.test(top.location.pathname));
 $('.product-option-selection-title').click(function(e) {
   $('.product-option-list').toggleClass('visible');
 });
+
+var nav_position = $('.header-nav-container').offset().top;
+var header_nav_height = $('.header-nav-container').outerHeight();
+
+$(window).on("scroll", function(e) {
+  if ($(window).scrollTop() > nav_position) {
+    $('.header-nav-container').addClass("fixed");
+    $('body').css('padding-top',header_nav_height+'px');
+  } else {
+     $('.header-nav-container').removeClass("fixed");
+     $('body').css('padding-top',0);
+  }
+
+});
