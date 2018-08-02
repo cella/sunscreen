5ba2a47800469ce7dc77eedc846eb48bed22316f
Nick Endle <nick.endle@gmail.com>
2018-08-02 11:38:54 -0500

    Product page updates
    Refine product page styles / layout, stub in basic select option dropdown, and make it toggle when clicked

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -1,1 +1,5 @@
 var inPreview = (/\/admin\/design/.test(top.location.pathname));
+
+$('.product-option-selection-title').click(function(e) {
+  $('.product-option-list').toggleClass('visible');
+});
